b1decd51ec74359bd6157398f67b622912d550df
nst131 <snistyuk@gmail.com>
2020-02-18 15:17:02 +0300

    Inventory

---
+++ Archero/Assets/Scripts/Moduls/UI/GameMenu.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameMenu : MonoBehaviour
+{
+
+    public void Back()
+    {
+        SceneManager.LoadScene("Menu");
+    }
+
+    public void Play()
+    {
+        SceneManager.LoadScene("Archero");
+    }
+}
